70665da8f473817e94b4350f730d2a537454517f
Hubits-Cloud <Tobias.Belling@hotmail.com>
2024-02-12 11:35:24 +0100

    added more php to the index and more notes to sql

--- php & databases/index.php
+++ php & databases/index.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../css/reset.css" rel="stylesheet">
     <link href="../css/style.css" rel="stylesheet">

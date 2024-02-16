5562924f9ac08edd1ed7bd85c83107d375fc54b5
Hubits-Cloud <Tobias.Belling@hotmail.com>
2024-02-16 12:17:29 +0100

    added som notes

--- php & databases/includes/dbh.inc.php
+++ php & databases/includes/dbh.inc.php
@@ -1,11 +1,18 @@
 <?php 
-
+# makes a var with host name and db name
 $dsn = "mysql:host=localhost;dbname=myfirstdatabase";
+
+# Makes a var with db username
 $dbusername = "root";
+
+# Makes a var with db password
 $dbpassword = "";
 
 try {
+    # makes a var containing the PDO class that takes the above globals as arguments
     $pdo = new PDO($dsn, $dbusername, $dbpassword);
+
+    #
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo"Connection failed: ". $e->getMessage();

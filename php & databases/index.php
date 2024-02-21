56756dd7c8bde92aef9db8ba6e654944535e1e4c
Hubits-Cloud <Tobias.Belling@hotmail.com>
2024-02-21 13:17:44 +0100

    changed the index to be a page where you can select the other pages from

--- php & databases/index.php
+++ php & databases/index.php
@@ -7,11 +7,33 @@ declare(strict_types=1);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-control" content="no-cache">
     <link href="../css/reset.css" rel="stylesheet">
     <link href="../css/style.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="login-page">
+
+        <form action="includes/formhandler.inc.php" method="post">
+
+            <div class="admin-link">
+                Dommer login <a href="login.php">Klik her</a>
+            </div>
+
+            <div class="admin-link">
+                Delete account <a href="delete.php">Klik her</a>
+            </div>
+
+            <div class="admin-link">
+                Change account <a href="change.php">Klik her</a>
+            </div>
+            
+            <div class="admin-link">
+                Search <a href="search.php">Klik her</a>
+            </div>
+
+        </form>
+    </div>
 </body>
 </html>

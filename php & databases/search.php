18b635132ca921e351e1649c296df998fd86f15d
Hubits-Cloud <Tobias.Belling@hotmail.com>
2024-02-21 13:19:28 +0100

    NOT FINISHED. Made search.php to allow a user to search for other users

---
+++ php & databases/search.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-control" content="no-cache">
+    <link href="../css/reset.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form class="searchform" action="search.php" method="post">
+        
+        <label for="search">Search for user:</label>
+        <input id="search" type="text" name="usersearch" placeholder="Search...">
+        <button>Search</button>
+
+    </form>
+</body>
+</html>

f2e40373a0dd8d0c3d5a5d131029d18a13d9e568
Hubits-Cloud <Tobias.Belling@hotmail.com>
2024-02-23 10:47:23 +0100

    Completed signup.php

---
+++ php & databases/signup.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+include_once "config.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Cache-control" content="no-cache">
+    <link href="../css/reset.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div class="login-page">
+        <h1>Dommer</h1>
+        <form action="includes/formhandler.inc.php" method="post">
+            <div class="txt-field">
+                <input type="text" name="username" required>
+                <span></span>
+                <label>Brugernavn</label>
+            </div>
+            <div class="txt-field">
+                <input type="password" name="pwd" required>
+                <span></span>
+                <label>Password</label>
+            </div>
+            <div class="txt-field">
+                <input type="text" name="email" required>
+                <span></span>
+                <label>Email</label>
+            </div>
+            <input type="submit" value="Sign up">
+            <div class="admin-link">
+                Return to index <a href="index.php">Klik her</a>
+            </div>
+        </form>
+    </div>
+</body>
+</html>

2270c6a764e7f13c95d7ff93674d361ebc7c7a11
Hubits-Cloud <Tobias.Belling@hotmail.com>
2024-02-29 11:29:06 +0100

    added better notes

--- arrays.php
+++ arrays.php
@@ -12,22 +12,30 @@ declare(strict_types=1);
 </head>
 <body>
     <?php
+    
+    # makes an indexed array
     $fruits = [
         "Apple",
         "Banana",
         "Cherry"
     ];
     
+    # echoes banana since it is in index 1
     echo $fruits[1];
 
+    # adds orange to the end of the array
     $fruits[] = "Orange";
 
+    # echoes orange
     echo $fruits[3];
 
+    # splices the array from index 0 to on position more (to index 1), and removes all data inbetween, in this case that's only apple.
     array_splice($fruits, 0, 1);
 
+    # echoes cherry since cherry is now in postion 1 with banana at 0
     echo $fruits[1];
 
+    # makes an associative array
     $tasks = [
         "Laundry" => "Daniel",
         "Trash" => "Frida",
@@ -35,8 +43,10 @@ declare(strict_types=1);
         "Dishes" => "Bella"
     ];
 
+    # echoes Daniel, since laundry is the kay to daniel.
     echo $tasks['Laundry'];
 
+    # echoes the amount of data in the array, in this case 4
     echo count($tasks) .'';
 
     ?>
